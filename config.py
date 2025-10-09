fefcca6b292c80a72d18284f91a26361f3b0df80
ShamraizRajpoot1 <sshamraiz4@gmail.com>
2025-10-09 10:40:49 +0500

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,3 @@ NEWBOOK_API_BASE = os.getenv("NEWBOOK_API_BASE")
 NEWBOOK_API_TOKEN = os.getenv("NEWBOOK_API_TOKEN")
 API_KEY = os.getenv("API_KEY")
 REGION = os.getenv("REGION")
-headers = {
-    "Content-Type": "application/json",
-    "Authorization": f"Bearer {"instances_5d5a3e706c853fed32092cdb0ad27b4c"}"
-}
